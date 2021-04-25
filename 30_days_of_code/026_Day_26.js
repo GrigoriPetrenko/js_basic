62903971f424f7857c4f583ea492f1ea0b73d312
grigory <petrenkogrigory18@gmail.com>
2021-04-25 22:37:23 +0200

    Day 26

---
+++ 30_days_of_code/026_Day_26.js
@@ -0,0 +1,7 @@
+function processData(input) {
+    //Enter your code here
+    const [d1,m1,y1,d2,m2,y2] = input.split(/\s/).map(Number);
+    if (y1 !== y2) console.log(y1 > y2 ? 10000 : 0);
+    else if(m1 !== m2) console.log(m1 > m2 ? (m1 - m2) * 500 : 0);
+    else console.log(d1 > d2 ? (d1 - d2) * 15 : 0);
+    }
